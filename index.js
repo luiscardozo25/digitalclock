06b0b126543b347cac370d384e2c4cbcf0d5c743
luiscardozo25 <20522292+luiscardozo25@users.noreply.github.com>
2023-12-05 16:25:53 +0000

    application upload

---
+++ index.js
@@ -0,0 +1,51 @@
+showTime();
+function showTime() {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = 'AM';
+    let dd = date.getDay();
+    let mm = date.getMonth();
+    let yyyy = date.getFullYear();
+
+    //Set time format to 24H
+    if (h >= 12) {
+        session = 'PM';
+    }
+
+    if (h == 0) {
+        h = 24;
+    }
+
+    if (h > 24) {
+        h = h-24;
+    }
+
+    //Hour formatting
+    if (h < 10) {
+        h = "0" + h;
+    } 
+
+    if (m < 10) {
+        m = "0" + m;
+    } 
+
+    if (s < 10) {
+        s = "0" + s;
+    } 
+
+    //Date formatting
+    if (dd < 10) {
+        dd = "0" + dd;
+    } 
+
+    if (mm < 10) {
+        mm = "0" + mm;
+    } 
+
+    let time = h + ':' + m + ':' + s + " " + session + " " + "|" + " " + dd + "/" + mm + "/" + yyyy;
+
+    $('#digitalclock').html(time);
+    setTimeout(showTime, 1000);
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+This is a simple digital clock with a date display.
+
+This was developed using only HTML5, CSS3, Javascript and JQuerry.
